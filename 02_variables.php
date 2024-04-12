4712be24aac12a1ddd10ef686090696993112df7
Zhongli27 <kazuhajean27@gmail.com>
2024-04-12 16:51:45 +0800

    first commit

---
+++ 02_variables.php
@@ -0,0 +1,24 @@
+<?php
+$name = 'Conrado'; // String
+$age = 28; // Int
+$has_kids = false; // Boolean
+$cash_on_hand = 20.75; // Float
+
+
+// var_dump($cash_on_hand);
+
+// echo $name . ' is ' . $age . ' years old ';
+
+// echo "$name is $age years old";
+
+$x = '5' + '5';
+//var_dump($x);
+//echo 10 - 5;
+//echo 5 * 6;
+//echo 10 / 2;
+//echo 10 % 3;
+
+define('HOST', 'localhost');
+define('DB_NAME', 'dev_db');
+
+echo HOST;
---
+++ 04_conditionals.php
@@ -0,0 +1,48 @@
+<?php
+$age = 20;
+//if($age >= 18) {
+    //echo 'You are old enough to vote';
+//}else{
+   // echo 'Sorry, you are not old enough to vote';
+//}
+
+/*$t = date("H");
+
+if($t < 12){
+    echo 'Good Morning';
+}else if($t < 17){
+    echo 'Good Afternoon';
+}else{
+    echo 'Good Evening';
+}*/
+
+$posts = [];
+
+/*if(!empty($posts)){
+    echo $posts[0];
+}else{
+    echo 'No Posts';
+}*/
+
+//echo !empty($posts) ? $posts[0] : 'No Posts';
+
+//$firstPost = !empty($posts) ? $posts[0] : 'No Posts';
+
+/*$firstPost = $post[0] ?? null;
+echo $firstPost;*/
+
+$favcolor = 'yellow';
+
+switch($favcolor){
+    case 'red':
+        echo 'Your favorite color is red';
+        break;
+    case 'blue':
+        echo 'Your favorite color is blue';
+        break;
+    case 'green':
+        echo 'Your favorite color is green';
+            break;
+    default:
+        echo 'Your favorite color is not red, green or blue';
+}
